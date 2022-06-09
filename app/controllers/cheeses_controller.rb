002292bb7561935ea3d31b8464e1a533a249096c
Courtney Garcia <wavesofgrace3@gmail.com>
2022-06-09 13:59:19 -0700

    commit tree 825dd08e15b145357c517d5cd04f8ae5d0b97564

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+   # find a cheeses using the ID from the URL
+   cheese = Cheese.find_by(id: params[:id])
+   # send a JSON response using that cheese objest
+   render json: cheese
+  end
+
 end
